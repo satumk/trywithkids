45d387b2953907d2d5cf9a03f13e94b784ec8d4e
satu <satumm@gmail.com>
2019-02-25 13:42:45 +0200

    revision 1 - jacoco, junit ja git. Hyödynnetty muistavanlaskurin koodia ja testejä.

---
+++ src/main/java/trywithkids/domain/TryWithKids.java
@@ -0,0 +1,34 @@
+
+package trywithkids.domain;
+
+
+public class TryWithKids {
+    //Ilmentymä muistaa tapahtumia-kentän suurimman arvon
+	private int tapahtumia, maxTapahtumia;
+	public TryWithKids(int alkuarvo){
+		if(alkuarvo < 0)
+			return;
+		this.tapahtumia = alkuarvo;
+		this.maxTapahtumia = alkuarvo;
+	}
+	public TryWithKids(){
+		this.tapahtumia = 0;
+	}
+
+  	public void lisaa(){
+    		this.tapahtumia = this.tapahtumia + 1;
+		if(tapahtumia > this.maxTapahtumia)
+			this.maxTapahtumia = tapahtumia; 
+  	}
+  	public int lukema(){
+    		return this.tapahtumia;
+  	}
+  	public void nollaa(){
+    		this.tapahtumia = 0;
+  	}
+        @Override
+	public String toString(){
+		return "Laskurin lukema on " + this.tapahtumia + " (max. " + this.maxTapahtumia +")";
+	}
+
+}
---
+++ src/test/java/trywithkids/domain/TryWithKidsTest.java
@@ -0,0 +1,44 @@
+
+package trywithkids.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TryWithKidsTest {
+
+    TryWithKids m;
+
+    @Before
+    public void setUp() {
+        m = new TryWithKids();
+    }
+
+    @Test
+    public void parametritonKonstruktoriAlkuarvoNolla() {
+        assertEquals(0, m.lukema());
+    }
+
+    @Test
+    public void alkuarvoEiNegatiivinen() {
+        TryWithKids m2 = new TryWithKids(0);
+        assertEquals(0, m2.lukema());
+    }
+
+    @Test
+    public void alkuarvoNegatiivinen() {
+        TryWithKids m2 = new TryWithKids(-1);
+        assertEquals(0, m2.lukema());
+    }
+
+    @Test
+    public void lisaaToimii() {
+        m.lisaa();
+        assertEquals(1, m.lukema());
+    }
+   
+    @Test
+    public void tamaEpaonnistuu() {
+        fail();
+    }
+}
